2b26060aaf0ab461ca5c87614b7ac94f439fdc60
Udi h Bauman <dibaunaumh@gmail.com>
2021-10-13 16:08:58 -0700

    Rendering alias of current resident

--- bldg-client/Assets/_Scripts/ResidentController.cs
+++ bldg-client/Assets/_Scripts/ResidentController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Models;
+using TMPro;
 
 public class ResidentController : MonoBehaviour
 {
 
     private Resident resident;
+    private TMP_Text alias;
+    private bool initialized = false;
 
 
 
@@ -14,17 +17,20 @@ public class ResidentController : MonoBehaviour
     public void initialize(Resident model) {
         resident = model;
         Debug.Log("Initializing resident " + resident.alias + " at " + resident.location);
+        initialized = true;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        alias = this.gameObject.GetComponentInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (initialized) {
+            alias.text = resident.alias;
+        }
     }
 }
